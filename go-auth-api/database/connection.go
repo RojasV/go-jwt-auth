c119f8e0b513c0593e05dcddfb28bd6bffb1e43b
RojasV <vladimirpalermo@hotmail.com>
2024-01-23 23:20:08 -0300

    subindo versao final da api

---
+++ go-auth-api/database/connection.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"github.com/RojasV/go-jwt-auth.git/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("use suas credenciais malandro!"), &gorm.Config{})
+
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+}
